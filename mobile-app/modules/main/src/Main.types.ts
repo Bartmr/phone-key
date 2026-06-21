6c71a5a66b7f68bb302d8a0be9b46468c9950130
Bartolomeu Rodrigues <pbartmr@gmail.com>
2026-06-21 12:51:01 +0100

    commit

--- mobile-app/modules/main/src/Main.types.ts
+++ mobile-app/modules/main/src/Main.types.ts
@@ -1,12 +1,2 @@
-export type MessageReceivedEvent = {
-  message: string;
-};
-
-export type GattServerErrorEvent = {
-  reason: string;
-};
-
 export type MainModuleEvents = {
-    onMessageReceived: (event: MessageReceivedEvent) => void;
-    onGattServerError: (event: GattServerErrorEvent) => void;
 }
--- mobile-app/modules/main/src/MainModule.ts
+++ mobile-app/modules/main/src/MainModule.ts
@@ -3,8 +3,6 @@ import { NativeModule, requireNativeModule } from 'expo';
 import { MainModuleEvents } from './Main.types';
 
 declare class MainModule extends NativeModule<MainModuleEvents> {
-    startGattServer: () => Promise<void>;
-    stopGattServer: () => Promise<void>;
 }
 
 export default requireNativeModule<MainModule>('MainModule');
